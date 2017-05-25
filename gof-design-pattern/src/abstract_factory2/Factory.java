c599f743e73670fc8ed24fec32217aeb6acd404b
YongHoonJi <Justin@218.38.137.28>
2017-05-26 00:41:54 +0900

    add-builder

--- gof-design-pattern/src/abstract_factory2/Factory.java
+++
@@ -1,5 +0,0 @@
-package abstract_factory2;
-
-public interface Factory {
-	public Laptop assemble();
-}
--- gof-design-pattern/src/abstract_factory2/OsType.java
+++
@@ -1,7 +0,0 @@
-package abstract_factory2;
-
-public enum OsType {
-	MAC,
-	Chrome,
-	WINDOWS;
-}
